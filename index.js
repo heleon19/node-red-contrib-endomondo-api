6e2fb03d868f496a5026d948581f7733b05bcfab
heleon19 <rjenni@bluewin.ch>
2017-05-16 18:41:00 +0200

    Try to add a second node

--- index.js
+++ index.js
@@ -7,5 +7,16 @@ module.exports = function(RED) {
             node.send(msg);
         });
     }
+
+    function LowerCaseNode2(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+
     RED.nodes.registerType("Workouts",LowerCaseNode);
+    RED.nodes.registerType("Workouts2",LowerCaseNode2);
 }
